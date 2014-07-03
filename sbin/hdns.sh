63a991d3bb23c6596328383b7f601d6be26de3d9
hdcola <hd@HDiMac.local>
2014-07-03 08:38:38 +0800

    兼容0.5.33版ModouROM

--- sbin/hdns.sh
+++ sbin/hdns.sh
@@ -5,11 +5,22 @@ hdnsconf="conf-dir=/data/apps/hdns/conf";
 
 initconfig()
 {
+    # generate the modouwifi.net DNS record
     /bin/sed -ie "/address=\/modouwifi.net/d" $config;
     toip=`nvram_get 2860 lan_ipaddr`;
     strategy="address=/modouwifi.net/$toip";
+
+    # get hardware version
+    hwver=`/bin/sn_get hw`
+    swver=`/bin/cat /etc/version | /usr/bin/head -n 2 | /usr/bin/tail -n 1`
+    g2mac=`/sbin/ifconfig ra0 | /usr/bin/head -n 1 | /usr/bin/awk '{ print $5}'`
+    g5mac=`/sbin/ifconfig rai0 | /usr/bin/head -n 1 | /usr/bin/awk '{ print $5}'`
+
+    txtRecord="txt-record=all,hwver#$hwver,swver#$swver,2gmac#$g2mac,5gmac#$g5mac"
+
     /system/sbin/writesys.sh;
     echo $strategy > $config;
+    echo $txtRecord >> $config;
     echo $hdnsconf >> $config;
     /system/sbin/writesys.sh close;
 }
